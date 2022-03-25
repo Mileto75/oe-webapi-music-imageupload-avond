2b1e1bdfe02ea4b563a5c7f7a14f0995bbb3265e
Mileto Di Marco <mileto.di.marco@howest.be>
2022-03-25 13:07:14 +0100

    rework migration

--- Pri.WebApi.Music.Infrastructure/Migrations/20220325120649_first.Designer.cs
+++ Pri.WebApi.Music.Infrastructure/Migrations/20220325120649_first.Designer.cs
@@ -10,8 +10,8 @@ using Pri.Oe.WebApi.Music.Infrastructure.Data;
 namespace Pri.WebApi.Music.Infrastructure.Migrations
 {
     [DbContext(typeof(MusicDbContext))]
-    [Migration("20220325104431_First")]
-    partial class First
+    [Migration("20220325120649_first")]
+    partial class first
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -23,12 +23,13 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
 
             modelBuilder.Entity("Pri.Oe.WebApi.Music.Core.Entities.Album", b =>
                 {
-                    b.Property<Guid>("Id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier");
+                        .HasColumnType("int")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<Guid>("ArtistId")
-                        .HasColumnType("uniqueidentifier");
+                    b.Property<int>("ArtistId")
+                        .HasColumnType("int");
 
                     b.Property<string>("Image")
                         .HasColumnType("nvarchar(max)");
@@ -48,128 +49,128 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                     b.HasData(
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000001"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000001"),
+                            Id = 1,
+                            ArtistId = 1,
                             Image = "images/album/andjusticeforall.jpg",
                             Name = "...And Justice for All",
                             ReleaseDate = new DateTime(1988, 9, 7, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000002"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000001"),
+                            Id = 2,
+                            ArtistId = 1,
                             Image = "images/album/metallica.jpg",
                             Name = "Metallica",
                             ReleaseDate = new DateTime(1991, 8, 12, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000003"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000001"),
+                            Id = 3,
+                            ArtistId = 1,
                             Image = "images/album/masterofpuppets.jpg",
                             Name = "Master of Puppets",
                             ReleaseDate = new DateTime(1986, 3, 3, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000004"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000001"),
+                            Id = 4,
+                            ArtistId = 1,
                             Image = "images/album/hardwired.jpg",
                             Name = "Hardwired...To Self-Destruct",
                             ReleaseDate = new DateTime(2016, 11, 18, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000005"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000002"),
+                            Id = 5,
+                            ArtistId = 2,
                             Image = "images/album/appetitefordestruction.jpg",
                             Name = "Appetite For Destruction",
                             ReleaseDate = new DateTime(1987, 7, 21, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000006"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000002"),
+                            Id = 6,
+                            ArtistId = 2,
                             Image = "images/album/useyourillusion1.jpg",
                             Name = "Use Your Illusion I",
                             ReleaseDate = new DateTime(1991, 9, 17, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000007"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000002"),
+                            Id = 7,
+                            ArtistId = 2,
                             Image = "images/album/useyourillusion2.jpg",
                             Name = "Use Your Illusion II",
                             ReleaseDate = new DateTime(1991, 9, 17, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000008"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000003"),
+                            Id = 8,
+                            ArtistId = 3,
                             Image = "images/album/mtvunpluggedinnewyork.jpg",
                             Name = "MTV Unplugged In New York",
                             ReleaseDate = new DateTime(1994, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000009"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000003"),
+                            Id = 9,
+                            ArtistId = 3,
                             Image = "images/album/liveatreading.jpg",
                             Name = "Live at Reading",
                             ReleaseDate = new DateTime(2009, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000010"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000003"),
+                            Id = 10,
+                            ArtistId = 3,
                             Image = "images/album/nevermind.jpg",
                             Name = "Nevermind",
                             ReleaseDate = new DateTime(1991, 9, 26, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000011"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000004"),
+                            Id = 11,
+                            ArtistId = 4,
                             Image = "images/album/ten.jpg",
                             Name = "Ten",
                             ReleaseDate = new DateTime(1991, 8, 27, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000012"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000004"),
+                            Id = 12,
+                            ArtistId = 4,
                             Image = "images/album/spintheblackcirclelive.jpg",
                             Name = "Spin The Black Circle Live In Seattle '95",
                             ReleaseDate = new DateTime(1995, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000013"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000005"),
+                            Id = 13,
+                            ArtistId = 5,
                             Image = "images/album/livetheanciennebelgique.jpg",
                             Name = "Live @ The Ancienne Belgique",
                             ReleaseDate = new DateTime(2010, 4, 30, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000014"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000005"),
+                            Id = 14,
+                            ArtistId = 5,
                             Image = "images/album/blackfuel.jpg",
                             Name = "Black Fuel",
                             ReleaseDate = new DateTime(1997, 1, 27, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000015"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000007"),
+                            Id = 15,
+                            ArtistId = 7,
                             Image = "images/album/rageagainstthemachine.jpg",
                             Name = "Rage Against The Machine",
                             ReleaseDate = new DateTime(1992, 11, 3, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000016"),
-                            ArtistId = new Guid("00000000-0000-0000-0000-000000000007"),
+                            Id = 16,
+                            ArtistId = 7,
                             Image = "images/album/evilempire.jpg",
                             Name = "Evil Empire",
                             ReleaseDate = new DateTime(1996, 4, 16, 0, 0, 0, 0, DateTimeKind.Unspecified)
@@ -178,9 +179,10 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
 
             modelBuilder.Entity("Pri.Oe.WebApi.Music.Core.Entities.Artist", b =>
                 {
-                    b.Property<Guid>("Id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier");
+                        .HasColumnType("int")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<int>("Followers")
                         .HasColumnType("int");
@@ -201,7 +203,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                     b.HasData(
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000001"),
+                            Id = 1,
                             Followers = 15044763,
                             Image = "images/artist/metallica.jpg",
                             Name = "Metallica",
@@ -209,7 +211,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000002"),
+                            Id = 2,
                             Followers = 17664350,
                             Image = "images/artist/gunsnroses.jpg",
                             Name = "Guns N' Roses",
@@ -217,7 +219,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000003"),
+                            Id = 3,
                             Followers = 11175759,
                             Image = "images/artist/nirvana.jpg",
                             Name = "Nirvana",
@@ -225,7 +227,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000004"),
+                            Id = 4,
                             Followers = 5980792,
                             Image = "images/artist/pearljam.jpg",
                             Name = "Pearl Jam",
@@ -233,7 +235,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000005"),
+                            Id = 5,
                             Followers = 9637,
                             Image = "images/artist/channelzero.jpg",
                             Name = "Channel Zero",
@@ -241,7 +243,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000006"),
+                            Id = 6,
                             Followers = 78309,
                             Image = "images/artist/therapy.jpg",
                             Name = "Therapy?",
@@ -249,7 +251,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000007"),
+                            Id = 7,
                             Followers = 4468769,
                             Image = "images/artist/rageagainstthemachine.jpg",
                             Name = "Rage Against The Machine",
@@ -257,7 +259,7 @@ namespace Pri.WebApi.Music.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("00000000-0000-0000-0000-000000000008"),
+                            Id = 8,
                             Followers = 2141,
                             Image = "images/artist/kinghiss.jpg",
                             Name = "King Hiss",
